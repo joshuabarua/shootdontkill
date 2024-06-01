0c7085e799ba868dad0ef290a4f065a4baa69f0f
Joshua Barua <joshuabarua9@hotmail.com>
2024-06-01 21:16:58 +0200

    Add datenschutz, create footer, add to layout

---
+++ client/app/components/Footer.tsx
@@ -0,0 +1,44 @@
+import {CiMail, CiInstagram} from 'react-icons/ci';
+import {FaTiktok, FaInstagram} from 'react-icons/fa6';
+const Footer = () => {
+	const currentYear = new Date().getFullYear();
+	return (
+		<footer className="bg-white text-gray-800 py-12 flex flex-col gap-4 w-full">
+			<CiMail className="relative mx-12 " size={50} />
+			<div className="border-t-2  border-gray-800 "></div>
+			<div className="container grid grid-cols-1 lg:grid-cols-4 gap-10 mx-12 mt-8">
+				<div className="col-span-1 lg:col-span-2">
+					<h1 className="text-4xl">
+						<strong>SHOOT US A MESSAGE!</strong>
+					</h1>
+				</div>
+				<div className="flex flex-col gap-2">
+					<div className="text-xl pt-4">
+						<strong>CONTACT</strong>
+					</div>
+					<div className="flex flex-col ml-4">
+						<div className="text-sm mt-4">piewpiew@weshootbutdontkill.com</div>
+					</div>
+				</div>
+				<div className="flex flex-col">
+					<div className="text-xl pt-4">
+						<strong>FOLGEN</strong>
+					</div>
+					<div className="pt-4">
+						<div className="flex ml-4 gap-4">
+							<FaInstagram className="w-6 h-6" />
+							<FaTiktok className="w-6 h-6" />
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="flex items-center space-between gap-4 w-full px-12">
+				<div className="text-sm mt-4 w-1/2">© WE SHOOT BUT DONT KILL! {currentYear}</div>
+				<div className="text-sm mt-4 w-1/2 text-right">
+					<a href="https://weshootbutdontkill.com/datenschutzerklaerung/">IMPRINT</a>
+				</div>
+			</div>
+		</footer>
+	);
+};
+export default Footer;
---
+++ client/app/datenschutzerklarung/page.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+type Props = {};
+
+export default function Datenschutzerklärung({}: Props) {
+	return (
+		<div>
+			<strong>
+				<p>Impressum</p>
+			</strong>
+			<p>
+				Angaben gemäß § 5 TMG: We Shoot But Dont Kill! Pao Phunket 10551 Berlin Deutschland Kontakt: E-Mail: pao@weshootbutdontkill.com Umsatzsteuer-Identifikationsnummer gemäß
+				§27a UStG: DE323715262 Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV: Pao Phunket 12099 Berlin Deutschland Haftungsausschluss: Haftung für Inhalte Die Inhalte
+				unserer Website wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als
+				Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als
+				Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige
+				Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine
+				diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen
+				werden wir diese Inhalte umgehend entfernen. Haftung für Links Unsere Website enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben.
+				Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der
+				Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der
+				Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar.
+				Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen. Urheberrecht Die durch die Seitenbetreiber erstellten Inhalte und Werke auf
+				diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des
+				Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht
+				kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere
+				werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis.
+				Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen. Datenschutz: Datenschutzerklärung Verantwortliche Stelle im Sinne der
+				Datenschutzgesetze, insbesondere der EU-Datenschutzgrundverordnung (DSGVO), ist: Pao Phunket Musterstraße 123 12345 Musterstadt Deutschland Erfassung allgemeiner
+				Informationen Mit jedem Zugriff auf dieses Angebot werden durch uns bzw. den Webspace-Provider automatisch Informationen erfasst. Diese Informationen, auch als
+				Server-Logfiles bezeichnet, sind allgemeiner Natur und erlauben keine Rückschlüsse auf Ihre Person. Erfasst werden unter anderem: Name der Webseite, Datei, Datum,
+				Datenmenge, Webbrowser und Webbrowser-Version, Betriebssystem, der Domainname Ihres Internet-Providers, die sogenannte Referrer-URL (jene Seite, von der aus Sie auf
+				unser Angebot zugegriffen haben) und die IP-Adresse. Ohne diese Daten wäre es technisch teils nicht möglich, die Inhalte der Webseite auszuliefern und darzustellen.
+				Insofern ist die Erfassung der Daten zwingend notwendig. Darüber hinaus verwenden wir die anonymen Informationen für statistische Zwecke. Sie helfen uns bei der
+				Optimierung des Angebots und der Technik. Wir behalten uns zudem das Recht vor, die Log-Files bei Verdacht auf eine rechtswidrige Nutzung unseres Angebotes nachträglich
+				zu kontrollieren. Kontaktformular Treten Sie per E-Mail oder Kontaktformular mit uns in Kontakt, werden die von Ihnen gemachten Angaben zum Zwecke der Bearbeitung der
+				Anfrage sowie für mögliche Anschlussfragen gespeichert. Löschung bzw. Sperrung der Daten Wir halten uns an die Grundsätze der Datenvermeidung und Datensparsamkeit. Wir
+				speichern Ihre personenbezogenen Daten daher nur so lange, wie dies zur Erreichung der hier genannten Zwecke erforderlich ist oder wie es die vom Gesetzgeber
+				vorgesehenen vielfältigen Speicherfristen vorsehen. Nach Fortfall des jeweiligen Zweckes bzw. Ablauf dieser Fristen werden die entsprechenden Daten routinemäßig und
+				entsprechend den gesetzlichen Vorschriften gesperrt oder gelöscht. Ihre Rechte auf Auskunft, Berichtigung, Sperre, Löschung und Widerspruch Sie haben das Recht,
+				jederzeit Auskunft über Ihre bei uns gespeicherten personenbezogenen Daten zu erhalten. Ebenso haben Sie das Recht auf Berichtigung, Sperrung oder, abgesehen von der
+				vorgeschriebenen Datenspeicherung zur Geschäftsabwicklung, Löschung Ihrer personenbezogenen Daten. Bitte wenden Sie sich dazu an unseren Datenschutzbeauftragten. Die
+				Kontaktdaten finden Sie ganz unten. Damit eine Sperre von Daten jederzeit berücksichtigt werden kann, müssen diese Daten zu Kontrollzwecken in einer Sperrdatei
+				vorgehalten werden. Sie können auch die Löschung der Daten verlangen, soweit keine gesetzliche Archivierungsverpflichtung besteht. Soweit eine sol che Verpflichtung
+				besteht, sperren wir Ihre Daten auf Wunsch. Sie können Änderungen oder den Widerruf einer Einwilligung durch entsprechende Mitteilung an uns mit Wirkung für die Zukunft
+				vornehmen. Änderung unserer Datenschutzbestimmungen Wir behalten uns vor, diese Datenschutzerklärung gelegentlich anzupassen, damit sie stets den aktuellen rechtlichen
+				Anforderungen entspricht oder um Änderungen unserer Leistungen in der Datenschutzerklärung umzusetzen, z. B. bei der Einführung neuer Services. Für Ihren erneuten
+				Besuch gilt dann die neue Datenschutzerklärung. Fragen an den Datenschutzbeauftragten Wenn Sie Fragen zum Datenschutz haben, schreiben Sie uns bitte eine E-Mail oder
+				wenden Sie sich direkt an die für den Datenschutz verantwortliche Person in unserer Organisation: Pao Phunket 12099 Berlin Deutschland
+			</p>
+		</div>
+	);
+}
